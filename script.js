a8db0fb1a9bf81b42c81fc73cd72c5e34c3b6e6f
JAY <jitupassi437@gmail.com>
2025-04-11 09:51:48 +0530

    script.js

--- script.js
+++ script.js
@@ -16,3 +16,31 @@ function generateQR() {
 
 
 }
+function downloadQR() {
+    let qrCanvas = document.querySelector("#qrcode canvas");
+    if (qrCanvas) {
+        let img = qrCanvas.toDataURL("image/png");
+        let link = document.createElement("a");
+        link.href = img;
+        link.download = "qrcode.png";
+        link.click();
+    } else {
+        alert("Generate a QR code first!");
+    }
+}
+
+function shareQR() {
+    let qrCanvas = document.querySelector("#qrcode canvas");
+    if (navigator.share && qrCanvas) {
+        qrCanvas.toBlob(blob => {
+            let file = new File([blob], "qrcode.png", { type: "image/png" });
+            navigator.share({
+                files: [file],
+                title: "QR Code",
+                text: "Scan this QR code!"
+            }).catch(console.error);
+        });
+    } else {
+        alert("Sharing not supported or QR code not generated.");
+    }
+}
